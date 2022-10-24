e2dcd1b4abfea8ad6e6a4a7f6859d442a78e586b
Кушнаренко Григорий <Gr4n777.yandex.ru>
2022-10-24 14:43:46 +0300

    Двумерные массивы и рекурсия

---
+++ Задача_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write($"Введите количество чисел: ");
+int Count = int.Parse(Console.ReadLine()!);
+
+int[] Array = new int[Count];
+
+for (int i = 0; i < Array.Length; i++)
+{
+    Console.Write($"Введите число {i+1}: ");
+    Array[i] = int.Parse(Console.ReadLine()!);
+}
+int count = 0;
+foreach (var item in Array)
+{
+if (item > 0)
+count++;
+}
+
+Console.Write($"\nКоличество чисел больше нуля: {count} ");
+
+Console.ReadLine();
---
+++ Задача_2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.*/
+
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b1 - b2)/(k2 - k1);
+double y = k2 * x + b2;
+
+Console.WriteLine($"Точка пересечения двух прямых: ({x}; {y})");
